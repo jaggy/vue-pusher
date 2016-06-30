2696505c6a8787e2ae7e6e02d0c5f7579b92aead
Jaggy Gauran <i.am@jag.gy>
2016-06-30 17:28:54 +0800

    Add a way to fetch all the channel subscriptions.

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 /**
- * VuePusher class
+ * VuePusher class.
+ *
  * @param {String} api_key
  * @param {Object} options
  */
 function VuePusher (api_key, options) {
     this.pusher   = new Pusher(api_key, options);
-    this.channels = {};
+    this.channels = [];
 }
 
 /**
  * Subscribe to the given channel and give a fallback to bind events to the channel.
  *
- * @param  {String}   channel
+ * @param  {String}   channel_name
  * @param  {Function} callback
  */
-VuePusher.prototype.subscribe = function (channel, callback) {
-    if (this.channels.hasOwnProperty(channel)) {
-        console.error('exists');
-        return;
+VuePusher.prototype.subscribe = function (channel_name, callback) {
+    var channel = this.pusher.subscribe(channel_name);
+
+    if (! this.channels.includes(channel)) {
+        this.channels.push(channel_name);
     }
 
-    this.channels[channel] = this.pusher.subscribe(channel);
-
-    callback(this.channels[channel]);
+    callback(channel);
 };
 
 /**
@@ -31,7 +31,16 @@ VuePusher.prototype.subscribe = function (channel, callback) {
  * @param  {String} channel
  */
 VuePusher.prototype.unsubscribe = function (channel) {
-    this.channels[channel].unsubscribe(channel);
+    this.pusher.unsubscribe(channel);
+};
+
+/**
+ * Return all the chanels.
+ *
+ * @return {Array}
+ */
+VuePusher.prototype.getChannels = function () {
+    return this.channels;
 };
 
 module.exports = {
