2463fd3d6b11fd7afa67604c963bacaaebc606e6
Jaggy Gauran <i.am@jag.gy>
2016-06-30 16:16:56 +0800

    Add a way to store channel subscriptions.

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
+/**
+ * VuePusher class
+ * @param {String} api_key
+ * @param {Object} options
+ */
+function VuePusher (api_key, options) {
+    this.pusher   = new Pusher(api_key, options);
+    this.channels = {};
+}
+
+/**
+ * Subscribe to the given channel and give a fallback to bind events to the channel.
+ *
+ * @param  {String}   channel
+ * @param  {Function} callback
+ */
+VuePusher.prototype.subscribe = function (channel, callback) {
+    if (this.channels.hasOwnProperty(channel)) {
+        console.error('exists');
+        return;
+    }
+
+    this.channels[channel] = this.pusher.subscribe(channel);
+
+    callback(this.channels[channel]);
+};
+
+/**
+ * Unsubscribe from the given channel.
+ *
+ * @param  {String} channel
+ */
+VuePusher.prototype.unsubscribe = function (channel) {
+    this.channels[channel].unsubscribe(channel);
+};
+
 module.exports = {
     install: function (Vue, options) {
-        var api_key = options.api_key;
-        var cluster = options.cluster;
+        var pusher = new VuePusher(options.api_key, options.options);
 
-        Vue.prototype.$pusher = new Pusher(api_key, {
-            cluster:   cluster,
-            encrypted: true
-        });
+        Vue.prototype.pusher  = pusher;
+        Vue.prototype.$pusher = pusher.pusher; // Just in case they want to manage it themselves.
     }
 };
